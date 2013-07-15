ab92b4da35a601686b7d31a3b64022f60069e044
Mark McDonald <i@ii.net>
2013-07-15 15:45:35 +0800

    added config options for toggling damage heatmap, map radius & resolution

--- exploratory.r
+++ exploratory.r
@@ -1,15 +1,21 @@
-# defaults
+# display settings
 # default energy by resonator level
 res.defaultenergy <- c(1000, 1500, 2000, 2500, 3000, 4000, 5000, 6000)
 # resonator colours, by level (stolen from IPAS)
 res.colors = c("#fece5a", "#ffa630", "#ff7315", "#e40000", "#fd2992", "#eb26cd", "#c124e0", "#9627f4")
+# radius of the map to show (max portal radius is 40m but you can expand for prettier charts that render slower)
+map.radius = 50
+# how fine the detail is on the heatmap (1 = 1m, 0.1 = 10cm)
+map.resolution = 0.1
+# show the heatmap background?  this is the slowest part of the process
+map.showheatmap = T
 
 # input data
 
 # ALL L1, 35m distance
 # level, 1-8, use NA for missing res
 #res.level <- rep(1, 8)
-# distance from portal center, in m, 1-40
+# distance from portal center, in meters.  Ingress lets you place up to 40m away, usually about 35 at best though.
 #res.distance <- rep(35, 8)
 
 # LAZY LOADING, 1xL8 PLAYER, 35M DIST
@@ -98,34 +104,50 @@ graphracer <- function (x,y,bl,bx,by) {
 damage_func = graphracer
 
 # plot the background; burster damage
+par(pty="s") # keep the aspect ratio square
 
 # matrix & picture of burster damage
+matrix_size = map.radius * 1/map.resolution
 # 800x800 matrix for -40.0 to +40.0 w/0.1 increment steps
-dmg_matrix <- matrix(0, 800, 800)
+dmg_matrix <- matrix(0, 2 * matrix_size, 2 * matrix_size)
 res.symbols = rep(21, 8)
 res.energypct = rep("100%", 8)
 total_dmg = 0
 if (length(bursters.levels > 0)) {
   for (i in 1:length(bursters.levels)) {
-    for (x in -400:400) {
-      for (y in -400:400) {
-        dx = x / 10
-        dy = y / 10
-        dmg_matrix[x+400,y+400] = dmg_matrix[x+400,y+400] + 
-          damage_func(dx,dy, bursters.levels[i], bursters.x[i],bursters.y[i])
+    if (map.showheatmap) {
+      for (x in -matrix_size:matrix_size) {
+        for (y in -matrix_size:matrix_size) {
+          dx = x * map.resolution
+          dy = y * map.resolution
+          dmg_matrix[x+matrix_size,y+matrix_size] = dmg_matrix[x+matrix_size,y+matrix_size] + 
+            damage_func(dx,dy, bursters.levels[i], bursters.x[i],bursters.y[i])
+        }
+      }
+      image(dmg_matrix, frame.plot=F, axes=F)
+      par(new=T) # persist previous image & d
+    }
+    else {
+      # just calculate damage at each of the resonators
+      for (r in 1:8) {
+        x = round(res.getx(res.distance[r], r) / map.resolution)
+        y = round(res.gety(res.distance[r], r) / map.resolution)
+        dx = x * map.resolution
+        dy = y * map.resolution
+        dmg_matrix[x+matrix_size,y+matrix_size] = dmg_matrix[x+matrix_size,y+matrix_size] + 
+            damage_func(dx,dy, bursters.levels[i], bursters.x[i],bursters.y[i])
       }
     }
   }
-  image(dmg_matrix, frame.plot=F, axes=F)
-  par(new=T) # persist previous image & draw over it
-
+  
   # calculate damage on the resonators
   for (i in 1:8) {
     if (!is.na(res.level[i])) {
       x = res.getx(res.distance[i], i)
       y = res.gety(res.distance[i], i)
-      # pull damage from the matrix, capping out at the resonator's energy level
-      dmg = min(dmg_matrix[round(x*10)+400,round(y*10)+400], res.energy[i])
+      dmg = dmg_matrix[round(x/map.resolution)+matrix_size,round(y/map.resolution)+matrix_size]
+      # capping damage at the resonator's energy level
+      dmg = min(dmg, res.energy[i])
       # update resonator energy level
       new_energy = res.energy[i] - dmg
       res.energypct[i] = paste(round((new_energy / res.energy[i]) * 100), "%", sep="")
@@ -139,13 +161,12 @@ if (length(bursters.levels > 0)) {
 }
 
 # plot the resonators
-par(pty="s") # keep the aspect ratio square
 #plot(-40:40,-40:40,type="n",xaxs="i",yaxs="i") # draw an empty plot so we can get gridlines behind data points
 #axis(1, pos=0, col="#aaaaaa", col.axis="#aaaaaa")
 #axis(2, pos=0, las=2, col="#aaaaaa", col.axis="#aaaaaa")
 #par(new=T)
 plot(diag(sapply(res.distance, res.getx, 1:8)), diag(sapply(res.distance, res.gety, 1:8)), 
-     ylim=c(-40, 40), xlim=c(-40,40), xlab="", ylab="", xaxs="i", yaxs="i",
+     ylim=c(-map.radius, map.radius), xlim=c(-map.radius, map.radius), xlab="", ylab="", xaxs="i", yaxs="i",
      bg=res.colors[res.level], pch=res.symbols, cex=4, col="#ffffff")
 
 # plot the bursters
